18447678643723d29480486520beb45822100d9a
nick evans <nihluspower77@gmail.com>
2020-03-16 23:06:05 +0100

    Menu button javascript & rotate

---
+++ Dist/js/main.js
@@ -0,0 +1,33 @@
+//Select DOM Items
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set Initial Stae of Menu
+let showMenu = false;
+
+menuBtn.removeEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //Set Menu State
+        showMenu = True;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+        //Set Menu State
+        showMenu = False;
+
+    }
+}
